ae375fbe47a50292014c9e04232fc3c23041a770
Serge Ridge <serge.ridge@gmail.com>
2019-03-07 11:43:35 +0300

    update

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-import unittest
+from django.urls import reverse
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
-class NewVisitorTest(unittest.TestCase):
+class HomeNewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -12,10 +13,15 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_it_worked(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('Django', self.browser.title)
+    def get_full_url(self, namespace):
+        return self.live_server_url + reverse(namespace)
 
+    def test_home_title(self):
+        self.browser.get(self.get_full_url("home"))
+        self.assertIn("TaskBuster", self.browser.title)
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+    def test_h1_css(self):
+        self.browser.get(self.get_full_url("home"))
+        h1 = self.browser.find_element_by_tag_name("h1")
+        self.assertEqual(h1.value_of_css_property("color"),
+                         "rgba(200, 50, 255, 1)")
