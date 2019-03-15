9c7a581b782e7312250dd3d42a0b445cb0637f2f
Serge Ridge <serge.ridge@gmail.com>
2019-03-15 13:55:29 +0300

    update

--- taskbuster/settings/production.py
+++ taskbuster/settings/production.py
@@ -3,3 +3,16 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
+    }
+}
